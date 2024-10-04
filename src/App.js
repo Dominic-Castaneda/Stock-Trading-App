0aed9f03bce4ff202f8ae2223aa611895f237ba9
Dominic Castaneda <133822774+Dominic-Castaneda@users.noreply.github.com>
2024-10-04 13:43:14 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to the Stock Trading Simulator</h1>
+        <p>Stay tuned for our upcoming features!</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
